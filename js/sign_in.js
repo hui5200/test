9e9eec5ad8e86aa7aa993d75b5e492ce5e840a7e
linhui <linhui@akuvox.com>
2024-06-06 11:05:43 +0800

    考勤测试

--- js/sign_in.js
+++ js/sign_in.js
@@ -1,6 +1,6 @@
 function getTime() {
     const now = new Date();
-    return now.toLocaleTimeString('zh-CN', {
+    return now.toLocaleTimeString({
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
